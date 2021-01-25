33dfc0b29778ed848f7b127c4b99067f3cc00556
Paulo Marques <pauloedums@gmail.com>
2021-01-25 17:45:47 -0300

    most of the code

---
+++ examples/albums.js
@@ -0,0 +1,8 @@
+global.fetch = require('node-fetch');
+
+import {searchAlbums} from '../src/search';
+
+const albums = searchAlbums('Incubus');
+
+albums.then(data => data
+    .albums.items.map(item => console.log(item.name)));
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import {
+  search,
+  searchAlbums,
+  searchPlaylists,
+  searchArtists,
+  searchTracks
+} from './search';
+import {
+  getAlbum,
+  getAlbumTracks,
+  getAlbums
+} from './album';
+
+export {
+  search,
+  searchAlbums,
+  searchPlaylists,
+  searchArtists,
+  searchTracks,
+  getAlbum,
+  getAlbumTracks,
+  getAlbums
+}
