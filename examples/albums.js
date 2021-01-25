d61c7bb354c1c8fd49f9d8f951a2df8c8c1e1d0c
Paulo Marques <pauloedums@gmail.com>
2021-01-25 20:09:29 -0300

    resolving test problems and refactoring

--- examples/albums.js
+++ examples/albums.js
@@ -1,8 +1,10 @@
 global.fetch = require('node-fetch');
 
-import {searchAlbums} from '../src/search';
-
-const albums = searchAlbums('Incubus');
+import SpotifyWrapper from '../src/index';
+const spotify = new SpotifyWrapper({
+  token: 'BQDyk12MabW0hhcyUQ8uBAqdWs5uEBTOrIzm0xHggBHoZoxj6Rh1nI0UjNlmEN_61S3aFfmGi_4xJPh8BxNfmbleJzyFJz-cmRpgviEK42vx6HKDNF2rHjal-VHLdiCnrnybStURdjrwyjS5JQ8FiQRMCPM9bU7515c'
+})
+const albums = spotify.search.albums('Incubus');
 
 albums.then(data => data
     .albums.items.map(item => console.log(item.name)));
